cef447f72927985f98cd2ad0354e264e839047bd
Shant15 <133233366+Shant15@users.noreply.github.com>
2023-05-24 22:59:11 +0530

    Add files via upload

---
+++ src/robot.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name:'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howel',
+        username: 'Antonette',
+        email:'shanna@elissa.tv'
+    },
+    {
+        id: 3,
+        name:'clementina Bauch',
+        username: 'Samantha',
+        email:'Nathan@yesenia@net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.oconner@kory.org'
+    },
+    {
+        id: 5,
+        name:'chelsey Dietrich',
+        username: 'Kamren',
+        email:'Lucio_Hettinger@annie.ca'
+    },
+    {
+        id: 6,
+        name:'Mrs. Dennis schulist',
+        username: 'Leopoldo_corkery',
+        email:'karley_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name:'kurtis weissnt',
+        username: 'Elwy skiles',
+        email:'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name:'Nicholas Runolfsdottir v',
+        username: 'Maxime_Nienow',
+        email:'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name:'Glenna Reichert',
+        username: 'Delphine',
+        email:'chaim_McDermott@dana.io'
+    },
+    {
+        id: 10,
+        name:'clementina DuBuque',
+        username: 'Moriah.stanton',
+        email:'Rey.Padberg@karina.biz'
+    },
+]
